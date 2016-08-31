ec79be96b49cd2aae0d0aef44d36a8917728df1a
Name1001 <Name1001@mycom>
2016-08-31 21:34:30 +0900

    tmp5

--- Tmp/src/Tmp5.java
+++ Tmp/src/Tmp5.java
@@ -1,9 +1,10 @@
 
-public class TmpMaster{
+public class Tmp5{
 	public static void main(String[] args){
 		// tmp modify
 		// tmp modify - cherry pick
 		// tmp modify - rename branch
 		// master last
+		// tmp5
 	}
 }
