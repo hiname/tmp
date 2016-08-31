d82f1f410086032b4c53545e4af42ac1781ea48b
Name1001 <Name1001@mycom>
2016-08-31 21:31:45 +0900

    tmp4

--- Tmp/src/Tmp3.java
+++
@@ -1,9 +0,0 @@
-
-public class Tmp3{
-	public static void main(String[] args){
-		// tmp modify
-		// tmp modify - cherry pick
-		// tmp modify - rename branch
-		// master last
-	}
-}
--- Tmp/src/Tmp4.java
+++ Tmp/src/Tmp4.java
@@ -1,9 +1,10 @@
 
-public class TmpMaster{
+public class Tmp4{
 	public static void main(String[] args){
 		// tmp modify
 		// tmp modify - cherry pick
 		// tmp modify - rename branch
 		// master last
+		// tmp4
 	}
 }
