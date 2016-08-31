9be2ffd5d7b56b0a5fe52386613f7dd43783d18a
Name1001 <Name1001@mycom>
2016-08-31 20:30:25 +0900

    back back

--- Tmp/src/Tmp.java
+++ Tmp/src/Tmp.java
@@ -2,7 +2,9 @@
 public class Tmp{
 	public static void main(String[] args){
 		// tmp modify
+// <<<<<<< Upstream, based on master
 		// tmp modify - cherry pick
 		// tmp modify - rename branch
+		// 14b3473 tmp modify-reword
 	}
 }
